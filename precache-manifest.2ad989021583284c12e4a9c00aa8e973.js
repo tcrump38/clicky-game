c639e62c42ea54431e242d3d7571c1acecad0c04
tcuddle <thcrump38@yahoo.com>
2019-03-11 16:31:19 -0500

    Updates

---
+++ precache-manifest.2ad989021583284c12e4a9c00aa8e973.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "9511c0bf64345aa3e45a6802881ea169",
+    "url": "/clicky-game/static/media/images (2).9511c0bf.jpeg"
+  },
+  {
+    "revision": "d6ce1d1d152ca48509f5e324cddaca0e",
+    "url": "/clicky-game/static/media/download (3).d6ce1d1d.jpeg"
+  },
+  {
+    "revision": "f8175458ac3d9fabcb66561c4216c092",
+    "url": "/clicky-game/static/media/download (2).f8175458.jpeg"
+  },
+  {
+    "revision": "265538ab0e4606d350e6",
+    "url": "/clicky-game/static/js/runtime~main.27874bd3.js"
+  },
+  {
+    "revision": "2b381d13e8060e1ccfa4",
+    "url": "/clicky-game/static/js/main.72dc4aad.chunk.js"
+  },
+  {
+    "revision": "d14408cb011c7113242e",
+    "url": "/clicky-game/static/js/2.4b8ce697.chunk.js"
+  },
+  {
+    "revision": "2b381d13e8060e1ccfa4",
+    "url": "/clicky-game/static/css/main.acbb27e9.chunk.css"
+  },
+  {
+    "revision": "a44e3d82647ba05506d08d30f8c9528d",
+    "url": "/clicky-game/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -1,1 +1,34 @@
-"use strict";var precacheConfig=[["/clicky-game/index.html","e289e98b2ff6bc8649853ef42f09fe9f"],["/clicky-game/static/css/main.1dedf6d8.css","fab9bb4f4b9f1d9487507e3ebcd723bc"],["/clicky-game/static/js/main.7ba124ab.js","415da6ec4d9d5a338fa2bf2bf9b7e69d"],["/clicky-game/static/media/download (2).f8175458.jpeg","f8175458ac3d9fabcb66561c4216c092"],["/clicky-game/static/media/download (3).d6ce1d1d.jpeg","d6ce1d1d152ca48509f5e324cddaca0e"],["/clicky-game/static/media/images (2).9511c0bf.jpeg","9511c0bf64345aa3e45a6802881ea169"]],cacheName="sw-precache-v3-sw-precache-webpack-plugin-"+(self.registration?self.registration.scope:""),ignoreUrlParametersMatching=[/^utm_/],addDirectoryIndex=function(e,t){var n=new URL(e);return"/"===n.pathname.slice(-1)&&(n.pathname+=t),n.toString()},cleanResponse=function(t){return t.redirected?("body"in t?Promise.resolve(t.body):t.blob()).then(function(e){return new Response(e,{headers:t.headers,status:t.status,statusText:t.statusText})}):Promise.resolve(t)},createCacheKey=function(e,t,n,r){var a=new URL(e);return r&&a.pathname.match(r)||(a.search+=(a.search?"&":"")+encodeURIComponent(t)+"="+encodeURIComponent(n)),a.toString()},isPathWhitelisted=function(e,t){if(0===e.length)return!0;var n=new URL(t).pathname;return e.some(function(e){return n.match(e)})},stripIgnoredUrlParameters=function(e,n){var t=new URL(e);return t.hash="",t.search=t.search.slice(1).split("&").map(function(e){return e.split("=")}).filter(function(t){return n.every(function(e){return!e.test(t[0])})}).map(function(e){return e.join("=")}).join("&"),t.toString()},hashParamName="_sw-precache",urlsToCacheKeys=new Map(precacheConfig.map(function(e){var t=e[0],n=e[1],r=new URL(t,self.location),a=createCacheKey(r,hashParamName,n,/\.\w{8}\./);return[r.toString(),a]}));function setOfCachedUrls(e){return e.keys().then(function(e){return e.map(function(e){return e.url})}).then(function(e){return new Set(e)})}self.addEventListener("install",function(e){e.waitUntil(caches.open(cacheName).then(function(r){return setOfCachedUrls(r).then(function(n){return Promise.all(Array.from(urlsToCacheKeys.values()).map(function(t){if(!n.has(t)){var e=new Request(t,{credentials:"same-origin"});return fetch(e).then(function(e){if(!e.ok)throw new Error("Request for "+t+" returned a response with status "+e.status);return cleanResponse(e).then(function(e){return r.put(t,e)})})}}))})}).then(function(){return self.skipWaiting()}))}),self.addEventListener("activate",function(e){var n=new Set(urlsToCacheKeys.values());e.waitUntil(caches.open(cacheName).then(function(t){return t.keys().then(function(e){return Promise.all(e.map(function(e){if(!n.has(e.url))return t.delete(e)}))})}).then(function(){return self.clients.claim()}))}),self.addEventListener("fetch",function(t){if("GET"===t.request.method){var e,n=stripIgnoredUrlParameters(t.request.url,ignoreUrlParametersMatching),r="index.html";(e=urlsToCacheKeys.has(n))||(n=addDirectoryIndex(n,r),e=urlsToCacheKeys.has(n));var a="/clicky-game/index.html";!e&&"navigate"===t.request.mode&&isPathWhitelisted(["^(?!\\/__).*"],t.request.url)&&(n=new URL(a,self.location).toString(),e=urlsToCacheKeys.has(n)),e&&t.respondWith(caches.open(cacheName).then(function(e){return e.match(urlsToCacheKeys.get(n)).then(function(e){if(e)return e;throw Error("The cached response that was expected is missing.")})}).catch(function(e){return console.warn('Couldn\'t serve response for "%s" from cache: %O',t.request.url,e),fetch(t.request)}))}});
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/clicky-game/precache-manifest.2ad989021583284c12e4a9c00aa8e973.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/clicky-game/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
